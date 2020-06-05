a785e429c16de85ecd7859c06aba8082c5eaf2c1
William White <willcwhite@gmail.com>
2020-06-05 16:07:52 -0400

    fix issue with tbody loop

--- newCloudflare.py
+++ newCloudflare.py
@@ -5,7 +5,7 @@ import re
 
 https = urllib3.PoolManager()
 #GET request to url, BeautifulSoup to return html table
-cf_page = https.request('GET','https://developers.cloudflare.com/waf/change-log/scheduled-changes/')
+cf_page = https.request('GET','https://developers.cloudflare.com/waf/change-log/2020-05-04/')
 cf_resp = cf_page.data.decode('utf-8')
 soup = BeautifulSoup(cf_resp,'html.parser')
 table = soup.find('table')
@@ -23,20 +23,29 @@ for header in thead.findAll('th'):
 numLists = len(tbody.findAll('tr'))
 depth = [[]]*numLists
 counter = 0
-
+myDict = {}
+for num in range(len(tbody.findAll('tr'))):
+    myDict[num] = []
+allTbody = tbody.findAll('tr')
 #loop through tbody, find all th and td. convert to strings, remove html tags, dynamically create lists depending on how many tr's
 for i in tbody.findAll('tr'):
-    info = tbody.findAll(['th','td'])
+    myList = []
+    test = str(i)
+    test.split("</tr>")
+    info = allTbody[counter]
     for data in info:
         strdata = str(data)
         noTag = re.sub('<\w+>|</\w+>','',strdata)
-        depth[counter].append(noTag)
-
+        if noTag == '\n':
+            continue
+        myDict[counter].append(noTag)
+          
     counter+= 1
 
 #zip header_row and spec_row into a dict to match each key with its corresponding value 
 # loop through tbody entries, for each one, zip to the headers list, output that to external file     
-for x in range(len(depth)):
-    final = dict(zip(header_row,depth[x]))
-    with open('/Users/willwhite/Documents/dataTests/test.json','w') as json_file:
-        json.dump(final,json_file)
+for set in range(len(myDict)):
+    final = dict(zip(header_row,myDict[set]))
+    print(final)
+    #with open('/Users/willwhite/Documents/dataTests/test.json','w') as json_file:
+     #   json.dump(final,json_file)
